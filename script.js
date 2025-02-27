d33ec0c4388c50503d81caa78f1f37f6f85f9c4c
Ayush Sharma <160806563+ayuxharma@users.noreply.github.com>
2025-02-28 00:11:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+console.log('This is javascript ka second lecture') ;
+
+var a = 5 ;
+var b = 6 ;
+var c = "Ayush" ;
+
+console.log(a+b+8) ;
+
+console.log(typeof a) ;
+console.log(typeof c) ;
+
+// we should let  
+
+// object is a user defined data type
+
+let x = "Ayush bhai" ;
+let y = 22 ;
+let z = 3.55 ;
+const p = true ;
+let q = undefined ;
+let r = null ; // type of null is object (why ?)
+
+console.log(x, y, z, p, q, r) ;
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r) ;
+
+let o = {
+    name : "Ayush" ,
+    "job role" : "SDE" ,
+    age : 19 
+}
+console.log(o) ;
+
+o.salary = "15 lpa" ;
+
+console.log(o) 
