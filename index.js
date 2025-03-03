9ede29f968206227b075d8ffe89edde9fd6b528e
Ayush Sharma <160806563+ayuxharma@users.noreply.github.com>
2025-03-03 23:56:02 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,58 @@
-console.log(`This is a strings tutorial`) ;
+let arr = [1, 2, 4, 5, 7] ;
 
-let a = "Ayush" ;
+console.log(arr) ;
+console.log(arr.length) ;
+console.log(arr[2]);
 
-console.log(a[2]) ; // u
-console.log(a.length) ; // 5
+arr[1] = 69 ;
+console.log(arr) ;
+console.log(typeof(arr)) ;
 
-// template literals
-let name = "billu boy" ;
-console.log(`My name is ${name}`) ;
+// covert array to string
+console.log(arr.toString()) ;
 
-let b = "sharma" ;
-console.log(b.toUpperCase()) ;
-console.log(b.toLowerCase()) ;
-console.log(b.length) ;
+arr.push(12) ;
+console.log(arr) ;
 
-// slice a string
-console.log(b.slice(1,5)) ;
+arr.push("bc") ;
+console.log(arr) ;
 
-console.log(b.replace("ma" , "77")) ;
-console.log(b.concat(a)) ;
+// see other methods in notes
+
+
+// loops
+
+// for loop
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    console.log(element) ;
+}
+
+// for each loop
+arr.forEach((value, index, arr) => {
+    console.log(value, index, arr) ;
+}) ;
+
+// for in loop
+// for (const key in object) {
+//     if (Object.hasOwnProperty.call(object, key)) {
+//         const element = object[key];
+
+//     }
+// }
+
+// for of loop
+// for (const iterator of object) {
+    
+// } 
+
+let arr3 = [1,3,6,7] ;
+
+const greaterThanFive = (e) => {
+    if (e>5){
+        return true ; 
+    }
+    return false ;
+}
+arr3.filter(greaterThanFive) ;
+console.log(arr3.filter(greaterThanFive)) ;
