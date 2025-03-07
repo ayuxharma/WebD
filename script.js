e278332c41ba31b4d98c042a82c9aba2666fc766
Ayush Sharma <160806563+ayuxharma@users.noreply.github.com>
2025-03-07 20:51:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-console.log(`Hii Ayush`) ;
+let a = document.querySelector(".box").innerHTML ;
+console.log(a) ;
 
-let boxes = document.getElementsByClassName("box") ;
-console.log(boxes) ;
+let b = document.querySelector(".container").innerHTML ;
+console.log(b) ;
 
-// boxes[2].style.backgroundColor = "yellow" ;
+let c = document.querySelector(".container").innerText ;
+console.log(c) ;
 
-// document.getElementById("colorBox").style.backgroundColor = "pink" ;
+let d = document.querySelector(".container").outerHTML ;
+console.log(d) ;
 
-document.querySelector(".box").style.backgroundColor = "green" ;
+let e = document.querySelector(".container").tagName;
+console.log(e) ;
+
+let f = document.querySelector(".container").nodeName ;
+console.log(f) ;
+
+let g = document.querySelector(".container").textContent ;
+console.log(g) ;
